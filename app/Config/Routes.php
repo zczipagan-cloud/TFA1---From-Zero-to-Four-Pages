62f84e605833207ab60afad860514e58fa7617d3
zczipagan-cloud <zczipagan@fit.edu.ph>
2026-09-19 00:06:28 +0800

    Delete app directory

--- app/Config/Routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use CodeIgniter\Router\RouteCollection;
-
-$routes->get('/', 'Pages::home');
-$routes->get('/about', 'Pages::about');
-$routes->get('/customers', 'Customers::index');
-$routes->get('/users', 'Users::index');
